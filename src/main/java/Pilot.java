f92240c54aa049ebe03b32362afd6e6765dad9cc
Lewis <lewisdsykes@gmail.com>
2022-09-09 17:32:16 +0100

    cleaned up code

--- src/main/java/Pilot.java
+++ src/main/java/Pilot.java
@@ -1,12 +1,7 @@
 public class Pilot {
-
     private String name;
-
     private Rank rank;
-
     private String licenseNumber;
-
-
     public Pilot(String name, Rank rank, String licenseNumber) {
         this.name = name;
         this.rank = rank;
