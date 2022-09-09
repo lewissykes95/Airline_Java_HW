d0be53a82b4e712f2b3aa421d0d840e1d7b82886
Lewis <lewisdsykes@gmail.com>
2022-09-09 11:08:53 +0100

    added the CabinCrewMember class

---
+++ src/main/java/CabinCrewMember.java
@@ -0,0 +1,11 @@
+public class CabinCrewMember {
+    private String name;
+
+    private String rank;
+
+    public CabinCrewMember(String name, String rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+
+}
