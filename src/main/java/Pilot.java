a0a897a43be7d19e10c23a5bf6e420374af59cef
Lewis <lewisdsykes@gmail.com>
2022-09-09 11:35:28 +0100

    added Pilot class

---
+++ src/main/java/Pilot.java
@@ -0,0 +1,15 @@
+public class Pilot {
+
+    private String name;
+
+    private Rank rank;
+
+    private String licenseNumber;
+
+
+    public Pilot(String name, Rank rank, String licenseNumber) {
+        this.name = name;
+        this.rank = rank;
+        this.licenseNumber = licenseNumber;
+    }
+}
